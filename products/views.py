2a34349872841ad25e4527c13721776787f8c9ef
abuaminuu <i.abuaminu@gmail.com>
2026-09-01 00:41:01 +0100

    bug: fix permissions for category viewset to isadminorreadonly

--- products/views.py
+++ products/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from products.models import Category, Product
+from products.serializers import CategorySerializer, ProductSerializer
+from products.permissions import IsVendorOrReadonly, IsAdminUserOrReadOnly
 
 # Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    # fetch all categiries
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    # restric modifications to admins, GET for everyone
+    permission_classes = [IsAdminUserOrReadOnly]
+
+
+class ProductViewSet (viewsets.ModelViewSet):
+
+    # JOIN 'category' and 'merchant' in 1 query, then 1 Executes extra optimized query to batch-fetch all related 'images'
+    queryset = Product.objects.select_related('category', 'merchant').prefetch_related('images').all()
+    
+    serializer_class = ProductSerializer
+    permission_classes = [IsVendorOrReadonly]
+
+    # inject merchant 
+    def perform_create(self, serializer):
+        # Automatically set merchant to the authenticated user sending the POST request
+        serializer.save(merchant=self.request.user)
+    
