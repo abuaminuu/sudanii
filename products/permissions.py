85d321f7db3949136b559248b805a2d24259a82a
abuaminuu <i.abuaminu@gmail.com>
2026-09-01 00:39:55 +0100

    bug: permissions for isadminorreadonly

--- products/permissions.py
+++ products/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission, SAFE_METHODS, IsAdminUser
 
 class IsVendorOrReadonly(BasePermission):
 
@@ -6,7 +6,7 @@ class IsVendorOrReadonly(BasePermission):
         if request.method in SAFE_METHODS:
             return True
 
-        # else for write operations
+        # else for write operations & prevent AnonymousUser
         return request.user.is_authenticated and request.user.role in ["admin", "vendor"]
     
     def has_object_permission(self, request, view, obj):
@@ -14,3 +14,8 @@ class IsVendorOrReadonly(BasePermission):
             return True
         # only merchant and requst.user can modify data
         return obj.merchant == request.user
+    
+class IsAdminUserOrReadOnly(IsAdminUser):
+    def has_permission(self, request, view):
+        is_admin = super().has_permission(request, view)
+        return request.method in SAFE_METHODS or is_admin
