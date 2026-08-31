f55d242a2108e29437378bf3e4b35ce3151f6759
abuaminuu <i.abuaminu@gmail.com>
2026-08-31 20:59:27 +0100

    fix: picture and role inputs

--- accounts/models.py
+++ accounts/models.py
@@ -4,15 +4,17 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     roles = (
-        ("Admin","admin"),
-        ("Vendor","vendor"),
-        ("Customer","customer"),
+        ("admin","Admin"),
+        ("vendor","Vendor"),
+        ("customer","Customer"),
     )
 
     email = models.EmailField(unique=True)
-    role = models.CharField(max_length=8, choices=roles, default="customer")
+    role = models.CharField(max_length=32, choices=roles, default="customer")
+    picture = models.ImageField(upload_to="accounts/profile/", null=True, blank=True)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
 
     def __str__(self):
         return f"{self.email} {self.role}"
+
