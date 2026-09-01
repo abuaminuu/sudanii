8d6b923626be8336fe442b444b362b5bb9171392
abuaminuu <i.abuaminu@gmail.com>
2026-09-01 02:18:49 +0100

    refactor: remove unnessary imports

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets, generics
+from accounts.serializers import UserRegistrationSerializer
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
