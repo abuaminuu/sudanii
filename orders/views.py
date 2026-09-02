d2db916575bf7a26090995fd5eb83edf7022ed75
abuaminuu <i.abuaminu@gmail.com>
2026-09-02 18:03:59 +0100

    feat: order viesets

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from orders.models import Order
+from orders.serializers import OrderSerializer
+from django.http import HttpResponse
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 
 # Create your views here.
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if getattr(user, "role", None) == "admin":
+            return Order.objects.prefetch_related("items__product").all()
+
+        # else user must be customer or vendor
+        return Order.objects.filter(customer=user).prefetch_related("items__product").all()
+    
