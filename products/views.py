ceda3bbc2e1d1b3666fa9af1d976a5ed628569be
abuaminuu <i.abuaminu@gmail.com>
2026-09-05 12:36:00 +0100

    feat: add cache to product views

--- products/views.py
+++ products/views.py
@@ -3,6 +3,10 @@ from rest_framework import viewsets
 from products.models import Category, Product
 from products.serializers import CategorySerializer, ProductSerializer
 from products.permissions import IsVendorOrReadonly, IsAdminUserOrReadOnly
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
+
 
 # Create your views here.
 
@@ -23,6 +27,16 @@ class ProductViewSet (viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     permission_classes = [IsVendorOrReadonly]
 
+    # Cache Product List endpoint for 15 minutes (Includes filter & cursor params)
+    @method_decorator(cache_page(60 * 15))
+    @method_decorator(vary_on_headers("Authorization"))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    @method_decorator(cache_page(60 * 60))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+    
     # inject merchant 
     def perform_create(self, serializer):
         # Automatically set merchant to the authenticated user sending the POST request
